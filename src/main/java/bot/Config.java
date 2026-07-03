683068a9530ef06add5ed909b4cccf2497607bcf
jochris <jonathandev780@gmail.com>
2026-07-03 15:28:59 +0800

    feat: set Config.SELF to true

--- src/main/java/bot/Config.java
+++ src/main/java/bot/Config.java
@@ -8,7 +8,7 @@ public final class Config {
     public static final String BOT_NAME = "JaWa Bot";
     public static final String PREFIX = ".";
     public static final String VERSION = "1.0.0";
-    public static final boolean SELF = false;
+    public static final boolean SELF = true;
     public static final String OWNER_NUMBER = "62895416602000";
     public static final long START_TIME = System.currentTimeMillis();
 
